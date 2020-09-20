164b68844215335a50fc28a91e5fc6096ed5a59e
Mark Rivera <Mriver1991@gmail.com>
2020-09-20 13:55:38 -0400

    finished queue

--- queue/singly_linked_list.py
+++ queue/singly_linked_list.py
@@ -37,35 +37,55 @@ class Node:
 # where the snake can grow based upon having more links in it
 
 class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
+    def __init__(self, node = None):
+        self.head = node
+        self.tail = node
+        self.length = 1 if node is not None else 0
 
     def __len__(self):
-        count = 0
-        current_node = self.head
+        return self.length
 
-        while current_node is not None:
-            count += 1
-            current_node = current_node.next_node
-        return count
-
-    def set_head(self, value):
+    def add_to_head(self, value):
         node = Node(value)
-        node.set_next(self.head)
-        self.head = node
+        self.length += 1
 
-    def printList(self): 
-        temp = self.head 
-        while (temp): 
-            print(temp.value)
-            temp = temp.next_node
+        if not self.head and not self.tail:
+            self.head = node
+            self.tail = node
+        else:
+            node.next_node = self.head
+            self.head = node
+
+    def add_to_tail(self, value):
+        node = Node(value)
+        self.length += 1
+       
+        if not self.head and not self.tail:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next_node = node
+            self.tail = node
+
+    def remove_head(self):
+        prev_head = self.head
+        if not self.head and not self.tail:
+            return
+        elif self.head == self.tail:
+            self.length -= 1
+            self.head = None
+            self.tail = None
+            return prev_head.value
+        else:
+            self.length -= 1
+            self.head = self.head.next_node
+            return prev_head.value
+
 
 # n1 = Node(1)
 # n2 = Node(2)
 # n3 = Node(3)
 # n4 = Node(4)
-
 
 # n1.set_next(n2) # n1.next_node = n2
 # n1.get_value() # => 2
